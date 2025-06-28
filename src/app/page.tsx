5e6eb8b3d9cbf6b652c1cc944aa0cb465c3dd25a
Pawal Karki <pawal_karki@icloud.com>
2025-06-28 14:13:03 +0545

    Feat(04: Update) : Finalizing the data access layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-"use client";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { Client } from "./api/trpc/[trpc]/client";
+import { Suspense } from "react";
 
-const Page = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.sagarmathaAPI.queryOptions({ text: "world" }));
+const Page = async () => {
+  const queryClient = getQueryClient();
+  //prefetch the query what is does is it will fetch the data from the server and store it in the query client
+  void queryClient.prefetchQuery(
+    trpc.sagarmathaAPI.queryOptions({ text: "Hello Prefetch" })
+  );
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      {JSON.stringify(data)}
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
